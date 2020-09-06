7053a0c63d596ac24629ffd1060c6540ec87b712
r2kd <r2kd@r2kd.com>
2020-09-05 18:14:56 -0700

    first commit

---
+++ Game.py
@@ -0,0 +1,46 @@
+import turtle as t
+import os
+
+dirPath = os.path.dirname(os.path.abspath(__file__)) 
+image_left = dirPath + "/car_left.gif"
+image_right = dirPath + "/car_right.gif"
+image_up = dirPath + "/car_up.gif"
+image_down = dirPath + "/car_down.gif"
+t.Screen().register_shape(image_left)
+t.Screen().register_shape(image_right)
+t.Screen().register_shape(image_up)
+t.Screen().register_shape(image_down)
+shape = t.Turtle()
+shape.shape(image_left)
+
+def up():
+    shape.shape(image_up)
+    shape.setheading(90)
+    shape.forward(10)
+
+def down():
+    shape.shape(image_down)
+    shape.setheading(270)
+    shape.forward(10)
+
+def left():
+    shape.shape(image_left)
+    shape.setheading(180)
+    shape.forward(10)
+
+def right():
+    shape.shape(image_right)
+    shape.setheading(0)
+    shape.forward(10)
+
+t.setup(500, 500)
+t.Screen().bgcolor("sky blue")
+t.color("green")
+t.turtlesize(3, 3, 2)
+t.listen()
+
+t.onkey(up,"Up")
+t.onkey(down,"Down")
+t.onkey(left,"Left")
+t.onkey(right,"Right")
+t.mainloop()
